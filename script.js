6a766f5e40eb1df32982a0e7031062294eeb8dcf
reinahzl <reinahazzel@gmail.com>
2025-07-14 14:01:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,46 @@ const materias = {
   "Informático Jurídico 1": { creditos: 6, previas: ["Constitucional", "Personas"] },
   "Ciencia Política": { creditos: 7, previas: [] },
   "Derechos Humanos": { creditos: 10, previas: ["Constitucional"] },
-  "Obligaciones y Contratos": { creditos: 14, previas: ["Constitucional", "Personas", "Historia del Derecho", "Bienes"] },
+
+  "Obligaciones y Contratos": { creditos: 14, previas: ["Bienes", "Constitucional", "Personas", "Historia del Derecho"] },
   "Economía, Derecho e Instituciones": { creditos: 8, previas: [] },
   "Intro Metodología Investigación": { creditos: 6, previas: [] },
-  "Procesal 1": { creditos: 12, previas: ["Constitucional", "Personas", "Historia del Derecho", "Bienes", "Derechos Humanos"] },
-  "Informático Jurídico 2": { creditos: 4, previas: ["Bienes", "Informático Jurídico 1"] }
+  "Procesal 1": { creditos: 12, previas: ["Bienes", "Constitucional", "Personas", "Historia del Derecho", "Derechos Humanos"] },
+  "Informático Jurídico 2": { creditos: 4, previas: ["Bienes", "Informático Jurídico 1"] },
+
+  "D. Penal - Parte General": { creditos: 7, previas: ["Bienes", "Intro Derecho Penal", "Derechos Humanos"] },
+  "Teoría de la Responsabilidad Civil": { creditos: 10, previas: ["Obligaciones y Contratos"] },
+  "Derecho Internacional Público": { creditos: 12, previas: ["Bienes", "Constitucional", "Personas", "Historia del Derecho", "Intro Fenómeno Jurídico"] },
+  "Trabajo y Seguridad Social 1": { creditos: 11, previas: ["Constitucional", "Obligaciones y Contratos", "Economía, Derecho e Instituciones"] },
+
+  "Contratos Especiales": { creditos: 12, previas: ["Teoría de la Responsabilidad Civil", "Obligaciones y Contratos"] },
+  "D. Penal - Parte Especial": { creditos: 10, previas: ["D. Penal - Parte General"] },
+  "Administrativo 1": { creditos: 6, previas: ["Bienes", "Constitucional", "Personas"] },
+  "Registral": { creditos: 4, previas: ["Bienes", "Personas"] },
+  "Práctica Profesional 1": { creditos: 14, previas: [] },
+
+  "Financiero 1": { creditos: 6, previas: ["Constitucional", "Administrativo 1", "Economía, Derecho e Instituciones", "D. Penal - Parte General"] },
+  "Procesal 2": { creditos: 12, previas: ["Constitucional", "Contratos Especiales", "Procesal 1", "D. Penal - Parte General"] },
+  "Comercial 1": { creditos: 14, previas: ["Obligaciones y Contratos", "Teoría de la Responsabilidad Civil", "Economía, Derecho e Instituciones"] },
+
+  "Minoridad, Adolescencia y Familia": { creditos: 6, previas: ["Constitucional", "Contratos Especiales", "Procesal 1"] },
+  "Comercial 2": { creditos: 12, previas: ["Constitucional", "Contratos Especiales", "Procesal 1", "Comercial 1"] },
+  "Seminario del Área": { creditos: 2, previas: [] },
+  "Familia Personal y Patrimonial": { creditos: 8, previas: ["Constitucional", "Contratos Especiales", "Procesal 1", "Comercial 1"] },
+  "Derecho Agrario": { creditos: 10, previas: ["Administrativo 1", "Contratos Especiales"] },
+  "Administrativo 2": { creditos: 0, previas: ["Administrativo 1"] },
+
+  "Financiero 2": { creditos: 7, previas: ["Financiero 1", "Constitucional"] },
+  "Sucesiones": { creditos: 6, previas: ["Contratos Especiales", "Procesal 1"] },
+
+  "Teoría del Derecho": { creditos: 8, previas: ["Administrativo 1", "Contratos Especiales", "Procesal 1", "Intro Fenómeno Jurídico"] },
+  "Trabajo y Seguridad Social 2": { creditos: 11, previas: ["Obligaciones y Contratos", "Teoría de la Responsabilidad Civil", "Economía, Derecho e Instituciones", "Trabajo y Seguridad Social 1"] },
+  "Consultorio Jurídico 1": { creditos: 11, previas: [] },
+
+  "Consultorio Jurídico 2": { creditos: 11, previas: [] },
+  "Financiamiento Empresarial": { creditos: 4, previas: ["Contratos Especiales", "Comercial 1", "Comercial 2"] },
+  "Derecho Internacional Privado": { creditos: 12, previas: ["Obligaciones y Contratos", "Comercial 1", "Comercial 2", "Familia Personal y Patrimonial", "Sucesiones", "Derecho Internacional Público", "Procesal 1", "Procesal 2"] },
+  "Situaciones Jurídicas Subjetivas": { creditos: 12, previas: ["Administrativo 1", "Administrativo 2"] }
 };
 
 let aprobadas = new Set();
@@ -66,4 +101,5 @@ window.onload = () => {
   crearMalla();
   actualizarCreditos();
 };
+
 
